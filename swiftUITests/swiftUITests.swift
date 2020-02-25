d29980a48f96bb236598af4f8ab4d110b8958628
Stanislaw Sobczyk <stanislaw@airnauts.com>
2020-02-25 17:07:00 +0100

    Initial Commit

---
+++ swiftUITests/swiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftUITests.swift
+//  swiftUITests
+//
+//  Created by Stanislaw Sobczyk on 2/25/20.
+//  Copyright © 2020 Stanislaw Sobczyk. All rights reserved.
+//
+
+import XCTest
+@testable import swiftUI
+
+class swiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
